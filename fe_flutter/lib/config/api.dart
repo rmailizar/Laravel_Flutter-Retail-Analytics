cd386c1ca4b37fed9025ae36e61992900aab20e2
rmailizar <rmailizar@gmail.com>
2025-12-25 22:32:56 +0700

    Final (Semoga)

--- fe_flutter/lib/config/api.dart
+++ fe_flutter/lib/config/api.dart
@@ -1,5 +1,12 @@
 class Api {
+  // Pakai ini untuk handphone (USB)
   static const String baseUrl = "http://192.168.100.3:8000/api";
+
+  // Pakai ini untuk emulator (Android Studio)
+  // static const String baseUrl = 'http://10.0.2.2:8000/api';
+
+  // Pakai ini untuk run di browser
+  // static const String baseUrl = 'http://127.0.0.1:8000/api';
 
   static const String login = "$baseUrl/login";
   static const String products = "$baseUrl/products";
